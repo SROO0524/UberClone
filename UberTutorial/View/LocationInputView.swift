b700e9433fc1cb59f34f7897a28daaea34006c10
SROO0524 <vmflstm847@naver.com>
2020-08-11 15:11:59 +0900

    19강: Location InputView Wrap Up

--- UberTutorial/View/LocationInputView.swift
+++ UberTutorial/View/LocationInputView.swift
@@ -29,6 +29,65 @@ class LocationInputView: UIView {
         return button
     }()
     
+    let titleLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Midum Kim"
+        label.textColor = .darkGray
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+    }()
+    
+    private let startLocationIndicatiorView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        return view
+    }()
+    
+    private let linkingView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .darkGray
+        return view
+    }()
+    
+    private let destinationIndicatiorView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    private lazy var startingLocationTextField : UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Current Location"
+        tf.backgroundColor = .groupTableViewBackground
+        tf.font = UIFont.systemFont(ofSize: 14)
+        
+        let paddingView = UIView()
+        paddingView.setDimensions(height: 30, width: 8)
+        tf.leftView = paddingView
+        tf.leftViewMode = .always
+        tf.isEnabled = false
+        
+        return tf
+    }()
+    
+    private lazy var destinationLocationTextField : UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Enter a destination.."
+        tf.backgroundColor = .lightGray
+        tf.returnKeyType = .search
+        tf.font = UIFont.systemFont(ofSize: 14)
+        
+        let paddingView = UIView()
+        paddingView.setDimensions(height: 30, width: 8)
+        tf.leftView = paddingView
+        tf.leftViewMode = .always
+
+        
+        return tf
+    }()
+
+    
+    
     
     
     //    MARK: LifeCycle
@@ -42,6 +101,31 @@ class LocationInputView: UIView {
         
         addSubview(backButton)
         backButton.anchor(top: topAnchor, left: leftAnchor, paddingTop: 44, paddingLeft:  12, width: 24, height: 25)
+        
+        addSubview(titleLabel)
+        titleLabel.centerY(inView: backButton)
+        titleLabel.centerX(inView: self)
+        
+        addSubview(startingLocationTextField)
+        startingLocationTextField.anchor(top: backButton.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 40,paddingRight: 40, height:  30)
+        
+        addSubview(destinationLocationTextField)
+        destinationLocationTextField.anchor(top: startingLocationTextField.bottomAnchor, left:  leftAnchor,right: rightAnchor, paddingTop: 12, paddingLeft:  40, paddingRight: 40, height:  30)
+        
+        addSubview(startLocationIndicatiorView)
+        startLocationIndicatiorView.centerY(inView: startingLocationTextField,leftAnchor: leftAnchor, paddingLeft:  20)
+        startLocationIndicatiorView.setDimensions(height: 6, width: 6)
+        startLocationIndicatiorView.layer.cornerRadius = 6 / 2
+        
+        addSubview(destinationIndicatiorView)
+        destinationIndicatiorView.centerY(inView: destinationLocationTextField,leftAnchor: leftAnchor, paddingLeft:  20)
+        destinationIndicatiorView.setDimensions(height: 6, width: 6)
+        destinationIndicatiorView.layer.cornerRadius = 6 / 2
+       
+        addSubview(linkingView)
+        linkingView.centerX(inView: startLocationIndicatiorView)
+        linkingView.anchor(top: startLocationIndicatiorView.bottomAnchor, bottom: destinationIndicatiorView.topAnchor, paddingTop: 4, paddingBotton: 4, width:  0.5)
+        
     }
     
     required init?(coder: NSCoder) {
